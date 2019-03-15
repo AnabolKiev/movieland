6ebb46d808685c953e2b8e0a2db7ed47ac5a89ea
AnabolKiev <Win3kSrv>
2019-03-15 16:52:05 +0200

    [b-4] Get movies by genre

--- src/main/java/com/anabol/movieland/web/controller/MovieController.java
+++ src/main/java/com/anabol/movieland/web/controller/MovieController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,11 @@ public class MovieController {
     @GetMapping(value = "/random", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Movie> getRandom() {
         return movieService.getRandom();
+    }
+
+    @JsonView(Views.Public.class)
+    @GetMapping(value = "/genre/{genreId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Movie> getByGenre(@PathVariable("genreId") int genreId) {
+        return movieService.getByGenreId(genreId);
     }
 }
