8681bb9b233d620664091915b49a58d905c9d79b
AnabolKiev <Win3kSrv>
2019-04-03 16:52:55 +0300

    ThreadLocal @Secured annotation DRAFT!!!

--- src/main/java/com/anabol/movieland/web/auth/interceptor/AuthInterceptor.java
+++ src/main/java/com/anabol/movieland/web/auth/interceptor/AuthInterceptor.java
@@ -3,9 +3,12 @@ package com.anabol.movieland.web.auth.interceptor;
 import com.anabol.movieland.entity.UserRole;
 import com.anabol.movieland.service.SecurityService;
 import com.anabol.movieland.web.auth.Session;
+import com.anabol.movieland.web.auth.UserHolder;
+import com.anabol.movieland.web.auth.annotation.Secured;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,19 +18,29 @@ import java.util.Optional;
 
 @Service
 @Slf4j
-public class UserRoleInterceptor implements HandlerInterceptor {
+public class AuthInterceptor implements HandlerInterceptor {
     private SecurityService securityService;
 
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) throws IOException {
-        if (request.getHeader("uuid") != null) {
-            Optional<Session> sessionOptional = securityService.getByToken(request.getHeader("uuid"));
-            if (sessionOptional.isPresent() && sessionOptional.get().getUser().getRole() == UserRole.USER) {
-                request.setAttribute("user", sessionOptional.get().getUser());
-                log.info("User authorized successfully to post review");
-                return true;
+        boolean isAuthorized = false;
+        HandlerMethod handlerMethod = HandlerMethod.class.cast(handler);
+        if (handlerMethod.hasMethodAnnotation(Secured.class)) {
+            if (request.getHeader("uuid") != null) {
+                Optional<Session> sessionOptional = securityService.getByToken(request.getHeader("uuid"));
+                if (sessionOptional.isPresent() && sessionOptional.get().getUser().getRole() == UserRole.USER) {
+                    UserHolder.setCurrentUser(sessionOptional.get().getUser());
+                    isAuthorized = true;
+                }
             }
+        } else {
+            isAuthorized = true;
+        }
+
+        if (isAuthorized) {
+            log.info("User authorized successfully to post review");
+            return true;
         }
         log.warn("Unauthorized attempt to post review");
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
